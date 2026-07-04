2ec026207900cdbc48337368dc9eac617312a47f
Chinmay Arankalle <ak.chinmay@gmail.com>
2026-07-04 21:28:17 +0530

    feature: Added generic root folder path

--- link_diver.sh
+++ link_diver.sh
@@ -5,4 +5,5 @@ export TELEGRAM_CHAT_ID=-5541707809
 export TODOIST_PROJECT_ID=6f66WhcMXCfXrJCP
 export TODOIST_TOKEN=88d0be9fa06c5730b078669387850f46c2836c75
 
-python3 /Users/akchinmay/Downloads/work/Rilfa/codes/link_diver/driver.py
+current_dir="$(pwd)"
+python3 "$current_dir"/driver.py
